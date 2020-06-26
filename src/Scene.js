b3811a69a01a79812e94ff167f1498f9887e79ec
Deniz Gürsoy <dgursoy@innova.com.tr>
2020-06-26 10:57:52 +0300

    scene is created

---
+++ src/Scene.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Canvas, extend } from 'react-three-fiber';
+import Controls from "./Controls";
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GridHelper } from "three";
+
+extend({ OrbitControls ,GridHelper})
+
+function Scene (props){
+    return(
+        <Canvas camera={{ position: [-120, 100, 200] }}>
+        <Controls enableDamping enablePan={false} dampingFactor={0.1} rotateSpeed={0.1} maxPolarAngle={Math.PI / 2} />
+        <gridHelper args={[1000, 30]} />
+        <ambientLight intensity={1} />
+        {props.children}
+      </Canvas>
+    );
+}
+
+export default Scene;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import ReactDOM from 'react-dom'
 import React, { Suspense, useState } from 'react'
 
-import { Canvas, extend } from 'react-three-fiber'
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GridHelper } from "three";
 
 import Model from "./Model"
-import Controls from "./Controls"
 
+import Scene from "./Scene"
 
 import './styles.css'
 
-extend({ OrbitControls ,GridHelper})
+
 
 function Box() {
   return (
@@ -23,21 +19,13 @@ function Box() {
   )
 }
 
-
-
-
-
-
 function App() {
   const [clicked, set] = useState(false)
   return (
     <>
-      <Canvas camera={{ position: [-120, 100, 200] }}>
-        <Controls enableDamping enablePan={false} dampingFactor={0.1} rotateSpeed={0.1} maxPolarAngle={Math.PI / 2} />
-        <gridHelper args={[1000, 30]} />
-        <ambientLight intensity={1} />
+      <Scene camera={{ position: [-120, 100, 200] }}>
         <Suspense fallback={<Box />}>{clicked && <Model file="Duck.gltf" />}</Suspense>
-      </Canvas>
+      </Scene>
       {!clicked && <button onClick={() => set(true)}>Load duck w/ 2s delay</button>}
     </>
   )
