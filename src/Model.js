fd6187b13ce7b75ec119297ae00f39a3ac6a48bf
Deniz Gürsoy <dgursoy@innova.com.tr>
2020-06-26 20:12:32 +0300

    light are added.

--- src/Model.js
+++ src/Model.js
@@ -2,6 +2,7 @@ import React from "react";
 import { unstable_createResource as createResource } from './react-cache/index'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import delay from 'delay'
+import { Vector3 } from "three";
 
 // Creates a cached async resource
 const path = '/assets/gltf/server_rack/'
@@ -11,6 +12,8 @@ function Model(props) {
   // Read from cache ... this will throw an exception which will be caught by <Suspense />
   const { scene } = resource.read(props.file)
   // It won't come to this point until the resource has been fetched
+  scene.position.set(0,1,0)
+  console.log(scene);
   return <primitive object={scene} />
 }
 
--- src/Scene.js
+++ src/Scene.js
@@ -10,13 +10,29 @@ extend({ OrbitControls, GridHelper })
 
 function Scene(props) {
     return (
-        <Canvas camera={{ position: [-120, 100, 200] }}>
+        <Canvas camera={{ position: [1, 1, -3] }}>
             <Controls enableDamping enablePan={false} dampingFactor={0.1} rotateSpeed={0.1} maxPolarAngle={Math.PI / 2} />
-            <gridHelper args={[1000, 30]} />
-            <ambientLight intensity={1} />
+            <gridHelper args={[20, 20]} />
+            <ambientLight intensity={5} />
+            <Light brightness={10} color={"white"} />
             {props.children}
         </Canvas>
     );
 }
+
+function Light({ brightness, color }) {
+    return (
+      <spotLight
+        width={3}
+        height={3}
+        color={color}
+        intensity={brightness}
+        position={[1, 1, -3]}
+        lookAt={[0, 0, 0]}
+        penumbra={4}
+        castShadow
+      />
+    );
+  }
 
 export default Scene;
