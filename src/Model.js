abb34852c7599ca1067d7dd80073b1ba3ef9f4bd
Deniz Gürsoy <dgursoy@innova.com.tr>
2020-06-27 09:28:45 +0300

    second object is added

--- src/Model.js
+++ src/Model.js
@@ -4,15 +4,15 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import delay from 'delay'
 
 // Creates a cached async resource
-const path = '/assets/gltf/server_rack/'
-const resource = createResource(file => new Promise(async res => (await delay(2000), new GLTFLoader().load(path + file, res))))
+
+const resource = createResource(file => new Promise(async res => (await delay(2000), new GLTFLoader().load( file, res))))
 
 function Model(props) {
   // Read from cache ... this will throw an exception which will be caught by <Suspense />
   const { scene } = resource.read(props.file)
   // It won't come to this point until the resource has been fetched
-  scene.position.set(0,1,0)
- 
+  scene.position.set(props.position.x,props.position.y,props.position.z)
+  scene.scale.set(props.scale.x,props.scale.y,props.scale.z)
   return <primitive object={scene} onClick={props.onClick} />
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import ReactDOM from 'react-dom'
 import Scene from "./Scene"
 import ModelLoader from './ModelLoader'
 import React,{useState} from "react"
+import { Vector3 } from 'three';
 
 function App() {
   const [selectedObject, setSelectedObject] = useState({});
 
  function renderElement(){
-    if(selectedObject.type == text)
-       return(
+    if(selectedObject.type === text){
+      return(
 
         <div>
         <h3>{text}</h3>
@@ -17,6 +18,17 @@ function App() {
        <h5>Router Sayisi : 9 </h5>
       </div>
        );
+    }else if (selectedObject.type!=null){
+      return(
+
+      <div>
+      <h3>{selectedObject.type}</h3>
+     <h5>TT Bina : Ankara </h5>
+     <h5>Boyut : En (0.011m) Boy (0.01m) Derinlik (0.05m) </h5>
+ 
+    </div> );
+    }
+    
      
 
  }
@@ -28,7 +40,8 @@ function App() {
 
     
       <Scene>
-        <ModelLoader file="scene.gltf" onClick={()=>{setSelectedObject({type:text})}}   />
+        <ModelLoader file="/assets/gltf/server_rack/scene.gltf" onClick={()=>{setSelectedObject({type:text})}}   position={new Vector3(0,1,0)}  scale={new Vector3(1,1,1)}  />
+        <ModelLoader file="/assets/gltf/router/scene.gltf" onClick={()=>{setSelectedObject({type:"D link modem is selected"})}}  position={new Vector3(2,0,0)}   scale={new Vector3(0.002,0.002,0.002)}  />
       </Scene>
       
       <div className="infoPanel">
